5b7a17bffac865be5f3d42be9c6853889725ec45
Shinto Theruvil Manuel <shinto.manuel@gmail.com>
2016-09-03 22:32:22 +0530

    added functionality

--- dna.py
+++ dna.py
@@ -19,3 +19,37 @@ The project should have methods for each of the following:
 '''
 
 sample = ['GTA','GGG','CAC']
+
+def read_dna(dna_file):
+  dna_data = ""
+  with open(dna_file, 'r') as f:
+    for line in f:
+      dna_data += line
+  return dna_data   
+
+def dna_codons(dna):
+  codons = []
+  for i in range(0, len(dna), 3):
+    if (i+3) < len(dna):
+      codons.append(dna[i:i+3])
+  return codons
+
+def match_dna(dna):
+  matches = 0
+  for codon in dna:
+    if codon in sample:
+      matches += 1
+  return matches
+
+def is_criminal(dna_sample):
+  dna_data = read_dna(dna_sample)
+  codons = dna_codons(dna_data)
+  num_matches = match_dna(codons)
+  
+  if num_matches >= 3:
+    print "%d matches found. Investigation could continue." % num_matches
+  else:
+    print "%d matches found. Suspect can be freed." % num_matches
+    
+
+is_criminal('suspect1.txt')
