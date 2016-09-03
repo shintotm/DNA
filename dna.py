2f9481eaa7e2bf0a581fe6efb36c442e11698ae4
Shinto Theruvil Manuel <shinto.manuel@gmail.com>
2016-09-03 14:53:52 +0530

    Description added

--- dna.py
+++ dna.py
@@ -1,1 +1,21 @@
+'''
+A spy deleted important files from a computer. There were a few witnesses
+at the scene of the crime, but no one is sure who exactly the spy was.
+Three possible suspects were apprehended based on surveillance video. Each
+suspect had a sample of DNA taken from them. The computer's keyboard was 
+also swabbed for DNA evidence and, luckily, one small DNA sample was 
+successfully retrieved from the computer's keyboard.
+
+Given the three suspects' DNA and the sample DNA retreived from the keyboard,
+it's up to you to figure out who the spy is!
+
+The project should have methods for each of the following:
+
+1) Given a file, read in the DNA for each suspect and save it as a string
+2) Take a DNA string and split it into a list of codons
+3) Iterate through a suspect's codon list to see how many of their codons
+ match the sample codons
+4) Pick the right suspect to continue the investigation on
+'''
+
 sample = ['GTA','GGG','CAC']
